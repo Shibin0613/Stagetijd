31bd0fb32e1a75b54a6b7db803a5b896919376b7
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2023-03-29 15:58:06 +0200

    dbconn

---
+++ dbconn.php
@@ -0,0 +1,18 @@
+<?php
+
+// DB Conn
+// $servername = "localhost";
+// $username = "";
+// $password = "";
+// $db = "";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "stagetijd";
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+
+if (!$conn) {
+    echo "Connection Failed";
+}
