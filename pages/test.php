b185d9e0c0cece86103e222417ca71a870f829ae
Timhamer <240159@edu.rocfriesepoort.nl>
2023-04-03 14:00:39 +0200

    Insert, update, select query

--- pages/test.php
+++ pages/test.php
@@ -15,10 +15,13 @@
 include("header.php");
 
 use Controllers\DB;
-
-$result = DB::select("SELECT * FROM users WHERE id = :driehoek", ['driehoek' => 1]);
-var_dump($result);
-
-$users = DB::select("SELECT * FROM users");
-var_dump($users);
+$naam = "Tim";
+$email = "tfh@gmail.com";
+$rol = 1;
+$result1 = DB::insert("INSERT INTO `users` (`naam`, `email`, `role`) VALUES (:naam, :email, :rol)", ['naam' => $naam, 'email' => $email, 'rol' => $rol, ]);
+$userid = 2;
+$active = 1;
+$result2 = DB::update("UPDATE `users` SET `active` = :active WHERE id = :userid", ['active' => $active, 'userid' => $userid]);
+$result3 = DB::select("SELECT * FROM users WHERE id = :userid", ['userid' => $userid]);
+var_dump($result3);
 
