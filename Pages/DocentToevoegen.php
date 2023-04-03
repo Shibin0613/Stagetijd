4fd636116c229401a94205fcd62d3639373c4c39
Timhamer <240159@edu.rocfriesepoort.nl>
2023-04-03 14:47:04 +0200

    CSS weg

--- Pages/DocentToevoegen.php
+++ Pages/DocentToevoegen.php
@@ -5,35 +5,6 @@
 
 <head>
   <title>Insert Data into Database</title>
-  <style>
-    label {
-      display: block;
-      margin-bottom: 10px;
-    }
-
-    input[type="text"],
-    select {
-      width: 100%;
-      padding: 10px;
-      margin-bottom: 20px;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      box-sizing: border-box;
-    }
-
-    input[type="submit"] {
-      background-color: #4CAF50;
-      color: white;
-      padding: 12px 20px;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-    }
-
-    input[type="submit"]:hover {
-      background-color: #45a049;
-    }
-  </style>
 </head>
 
 <body>
@@ -62,7 +33,7 @@ if(isset($_POST['submit'])){
     $role = 1;
     $active = 1;
     $guid = uniqid();
-$result1 = DB::insert("INSERT INTO `users` (`naam`, `email`, `role`, `active`, `activationcode`) VALUES (:naam, :email, :rol, :active, :activationcode)", ['naam' => $naam, 'email' => $email, 'rol' => $role, 'active' => $active, 'activationcode' => $guid]);
+$result1 = DB::insert("INSERT INTO `users` (`naam`, `email`, `role`, `active`, `activationcode`) VALUES (:naam, :email, :rol, :active, :activationcode)", ['naam' => $name, 'email' => $email, 'rol' => $role, 'active' => $active, 'activationcode' => $guid]);
 
    
 }
