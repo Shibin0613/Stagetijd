275ddcff6dd073877472e32e143cc5fce58c533f
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2023-04-04 10:19:47 +0200

    accountoverzicht

--- Pages/accountOverzicht.php
+++ Pages/accountOverzicht.php
@@ -1,23 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/gebruikertoevoegen.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<title>StudentenOverzicht</title>
+</head>
+<body>
 <?php
 include "header.php";
 use Controllers\DB;
-// Select all users with the correct role
+//select alle studenten
 $role = "1";
 $result = DB::select("SELECT * FROM users WHERE `role` = :rol", ['rol' => $role]);
 
 
 
-// Display users in boxes
-
-if ($result) {
-    foreach($result as $row) {
-        // var_dump($row['naam']);
-        echo '<div class="StudentOverzicht">' . $row["naam"] . '</div>';
-    }
-} else {
-    echo "No users found.";
-}
-
-// Close connection
 
 ?>
+
+<div class="container">
+    <div class="row">
+        </div class="col-sm"> 
+        <?php
+            if ($result) {
+            foreach($result as $row) {
+            // var_dump($row['naam']);
+            echo '<a href="docentenOverzicht.php?id=' . $row['id'] . '"><div class="StudentenOverzicht">' . $row['naam'] . '</div></a>';
+        }
+        } else {
+            echo "No users found.";
+        }
+
+        ?>
+        </div>
+    </div>
+</div>
