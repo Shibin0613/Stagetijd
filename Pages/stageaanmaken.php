b50c29a05a0eee393d594d6438f7dcb1c2e99813
ShibinPan <220617@edu.rocfriesepoort.nl>
2023-04-10 21:42:40 +0200

    email voor stageaanmaken, en standaard datum op vandaag

--- Pages/stageaanmaken.php
+++ Pages/stageaanmaken.php
@@ -70,6 +70,9 @@ if (isset($_POST['verstuur'])) {
 	$praktijkbegeleiderID = DB::insert($table, $data);
 	$studentID = $praktijkbegeleiderID - 1;
 
+	$naaml = $_POST['naam_leerling'];
+	$emaill = $_POST['email_leerling'];
+
 	$data = [
 		'bedrijf' => $_POST['bedrijf'],
 		'praktijkbegeleiderId' => $praktijkbegeleiderID,
@@ -79,6 +82,9 @@ if (isset($_POST['verstuur'])) {
 	];
 	$table = "stage";
 	$stage = DB::insert($table, $data);
+
+	$naamp = $_POST['naam_praktijkbegeleider'];
+	$emailp = $_POST['email_praktijkbegeleider'];
 
 	// // Send email to learner
 	// $subject_leerling = 'stage';
@@ -94,4 +100,56 @@ if (isset($_POST['verstuur'])) {
 
 	// Redirect to success page
 	// header('Location: homedocent.php');
+
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+    // Create email headers
+    $headers .='X-Mailer: PHP/' . phpversion();
+
+    $subject ="Er is een account voor je aangemaakt";
+    
+    $message ="<html><body>";
+    $message .="Beste $naaml, "."<br/>";
+    $message .="Er is een stage aangemaakt. Voordat je kan inloggen moet je het eerste een wachtwoord aanmaken, dan wordt je account pas geactiveerd. "."<br/><br/>";
+    $message .="Met onderstaande knop word je naar andere pagina gestuurd om je wachtwoord aan te maken."."<br/><br/>";
+    //veranderen de url als de hele website later onlinestaat
+    $message .="<a href='localhost/stagetijd/pages/ActiveerAccount.php?activationcode=$guid'><button style='background-color:lime !important; box-shadow:0px 0px 2px 2px; border-radius:10px;'>Account activeren</button></a>"."<br/><br/>";
+    $message .="Met vriendelijke groet, "."<br/>";
+    $message .="<img src='https://lh3.googleusercontent.com/4CoybolZm3Xr1WNZbGPF_ZoUDR_Yn1NmWuo23yYHiZCdtkNm9GJKRQ5ugGJ5Y2zOWfSjGk5izMIDqh67=w378-h189-rw' style='width: 20%;'>";
+    $message .="</body></html>";
+
+    mail($emaill,$subject,$message,$headers);
+
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+    // Create email headers
+    $headers .='X-Mailer: PHP/' . phpversion();
+
+    $subject ="Er is een account voor je aangemaakt";
+    
+    $message ="<html><body>";
+    $message .="Beste $naamp, "."<br/>";
+    $message .="Er is een stage aangemaakt. Voordat je kan inloggen moet je het eerste een wachtwoord aanmaken, dan wordt je account pas geactiveerd. "."<br/><br/>";
+    $message .="Met onderstaande knop word je naar andere pagina gestuurd om je wachtwoord aan te maken."."<br/><br/>";
+    //veranderen de url als de hele website later onlinestaat
+    $message .="<a href='localhost/stagetijd/pages/ActiveerAccount.php?activationcode=$guid'><button style='background-color:lime !important; box-shadow:0px 0px 2px 2px; border-radius:10px;'>Account activeren</button></a>"."<br/><br/>";
+    $message .="Met vriendelijke groet, "."<br/>";
+    $message .="<img src='https://lh3.googleusercontent.com/4CoybolZm3Xr1WNZbGPF_ZoUDR_Yn1NmWuo23yYHiZCdtkNm9GJKRQ5ugGJ5Y2zOWfSjGk5izMIDqh67=w378-h189-rw' style='width: 20%;'>";
+    $message .="</body></html>";
+
+    if (mail($emailp,$subject,$message,$headers)){
+      echo "<script>alert('Er zijn zowel voor leerling en praktijkbegeleider accounts aangemaakt, diegene krijgt een mail om zijn account te activeren')</script>";
+    ?>
+    <META HTTP-EQUIV="Refresh" CONTENT="0; URL=stageaanmaken.php">
+    <?php
+      //als het al bestaat, dan wordt de docent teruggestuurd naar de pagina met ingevulde 
+      //voornaam en achternaam, maar de email is dan leeg.
+    }else{
+      echo "<script>alert('Het is niet gelukt om een account aan te maken, probeer later opnieuw!')</script>";
+    ?>
+    <META HTTP-EQUIV="Refresh" CONTENT="0; URL=stageaanmaken.php">
+    <?php
+    }
 }
