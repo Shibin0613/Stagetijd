5338b298c54b87111463397b57c5cd7d954a797d
Timhamer <240159@edu.rocfriesepoort.nl>
2023-04-17 14:52:41 +0200

    Backend van weken tonen

--- pages/ToBeDeleted.php
+++ pages/ToBeDeleted.php
@@ -1,29 +1,26 @@
 <?php
+//Incluse the required files
 include('../functions/services.php');
 include "header.php";
 
+//Makes it able to use the functions inside the services
 $studentService = new StudentServices();
 $logService = new LogService();
 
+//Get user info, internship info, log info, task info, tag info and comments that are connected to the user.
 $user = $studentService->getUserBy(['id' => 36,]);
 
+//Defines which log needs to be shown depending on the internship
 $internshipFilter = 0;
-$weeknumber = 0;
 
+//Gets the internship object from the filter.
 $internship = $user->internship[$internshipFilter];
-$log = $internship->logboek[$weeknumber];
-$monday = $log->monday;
-$tuesday = $log->tuesday;
-$wednesday = $log->wednesday;
-$thursday = $log->thursday;
-$friday = $log->friday;
-// var_dump($user->internship[0]->logboek[0]->tuesday->task);
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
+<? if (isset($_GET['id'])) : ?>
+    
+<? endif ?>
 
 <head>
     <meta charset="UTF-8">
@@ -33,35 +30,37 @@ $friday = $log->friday;
 </head>
 
 <body>
-    <?= $user->name ?>
-    <br>
-    <?= $internship->company ?>
     <table>
-    <tr>
-        <th>Weeknummer</th>
-        <th>Maandag</th>
-        <th>Dinsdag</th>
-        <th>Woensdag</th>
-        <th>Donderdag</th>
-        <th>Vrijdag</th>
-        <th>Goedkeuren</th>
-    </tr>
-    <tr>
-        <th><?= $log->weekNumber ?></th>
-        <th><?php $logService->DayLoop($monday) ?></th>
-        <th><?php $logService->DayLoop($tuesday) ?></th>
-        <th><?php $logService->DayLoop($wednesday) ?></th>
-        <th><?php $logService->DayLoop($thursday) ?></th>
-        <th><?php $logService->DayLoop($friday) ?></th>
-        <th>Nee</th>
-    </tr>
+        <tr>
+            <th>Weeknummer</th>
+            <th>Maandag</th>
+            <th>Dinsdag</th>
+            <th>Woensdag</th>
+            <th>Donderdag</th>
+            <th>Vrijdag</th>
+            <th>Goedkeuren</th>
+        </tr>
+        <?php foreach ($internship->logboek as $key => $value) : ?>
+            <tr>
+                <td><?= $value->weekNumber ?></td>
+                <td><a href="ToBeDeleted.php?id=<?= $value->monday->id ?>">Maandag</td>
+                <td><a href="ToBeDeleted.php?id=<?= $value->tuesday->id ?>">Dinsdag</td>
+                <td><a href="ToBeDeleted.php?id=<?= $value->wednesday->id ?>">wednesday</td>
+                <td><a href="ToBeDeleted.php?id=<?= $value->thursday->id ?>">thursday</td>
+                <td><a href="ToBeDeleted.php?id=<?= $value->friday->id ?>">friday</td>
+                <?php switch ($value->approved) {
+                    case 0:
+                        echo "<td> Niet goedgekeurd</td>";
+                        break;
+                    case 1:
+                        echo "<td> Goedgekeurd</td>";
+                        break;
+                    default:
+                        echo "<td> Er is iets mis gegaan.</td>";
+                        break;
+                }?>
+            </tr>
+        <?php endforeach ?>
     </table>
-    <br>
-    
-  
-   
-    
-    
 </body>
-
 </html>
