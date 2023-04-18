edeb4c31c09ca8beefee84a7fbe51b15df7f0a59
Timhamer <240159@edu.rocfriesepoort.nl>
2023-04-18 10:52:54 +0200

    Totale uren zien en knopjes voor de dagen.

--- pages/header.php
+++ pages/header.php
@@ -19,7 +19,6 @@ DB::connect();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../Styles/style.css">
     <script src="https://kit.fontawesome.com/42b6daea05.js" crossorigin="anonymous"></script>
-    <title>header</title>
 </head>
 <body>
     
